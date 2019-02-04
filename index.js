e4ecc938bcf093398ac444ee5ca67993dce4ac65
Veronica Miller <vmcmiller@gmail.com>
2019-02-04 04:09:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,3 +9,9 @@ const returnLastTwoDrivers = function(drivers) {
 
 const selectingDrivers =
   [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = function(multiplier) {
+  return function(fare) {
+    return fare * multiplier;
+  };
+};
